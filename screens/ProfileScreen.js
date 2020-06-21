461653d91c1c0c7051989ee592fc49883b04fbf4
AbstractUltra <45088170+abstractultra@users.noreply.github.com>
2020-06-21 12:58:03 -0400

    changes

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, FlatList } from "react-native";
 import { Avatar, ButtonGroup, ListItem } from "react-native-elements";
 
 export default function ProfileScreen() {
+  const [selectedTimeframe, setTimeframe] = useState(0);
   const tasks = [
-    {
-      task: "Go do something",
-      checked: false,
-    },
-    {
-      task: "Eat a cookie",
-      checked: true,
-    },
-    {
-      task: "Go outside",
-      checked: false,
-    },
+    [
+      {
+        task: "Go do something",
+        checked: false,
+      },
+      {
+        task: "Eat a cookie",
+        checked: true,
+      },
+      {
+        task: "Go outside",
+        checked: false,
+      },
+    ],
+    [
+      {
+        task: "Go do everything",
+        checked: false,
+      },
+      {
+        task: "Eat a pizza",
+        checked: true,
+      },
+      {
+        task: "Go to your basement",
+        checked: false,
+      },
+    ],
+    [
+      {
+        task: "Go make a million dollars",
+        checked: false,
+      },
+      {
+        task: "Go to MIT",
+        checked: true,
+      },
+      {
+        task: "Go to the park",
+        checked: false,
+      },
+    ],
   ];
   return (
     <View style={{ flex: 1, backgroundColor: "#fff" }}>
@@ -30,10 +61,14 @@ export default function ProfileScreen() {
         />
         <Text style={{ fontSize: 30, fontWeight: "bold" }}>Leon Si</Text>
         <Text style={{ fontSize: 15 }}>Streak: 3 🔥</Text>
-        <ButtonGroup buttons={["Day", "Week", "Month"]} />
+        <ButtonGroup
+          buttons={["Day", "Week", "Month"]}
+          onPress={setTimeframe}
+          selectedIndex={selectedTimeframe}
+        />
       </View>
       <FlatList
-        data={tasks}
+        data={tasks[selectedTimeframe]}
         renderItem={({ item }) => (
           <ListItem title={item.task} checkBox={{ checked: item.checked }} />
         )}
